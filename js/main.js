16a4cc3a71189123e92aec4960e8b3d63b01977f
leos <leonardo.siri.email@gmail.com>
2019-10-20 15:02:35 +0200

    complete exercise

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ Il programma dovrà chiedere all’utente il numero di chilometri e l’età e d
 totale del viaggio. Il prezzo del biglietto è definito in base ai km (0.21 €cent al km) ma c’è uno 
 sconto del 20% per i minorenni e del 40% per gli over 65 */
 
-var kms, age, price, str;
+var kms, age, price;
 
 // the user is asked the kms' number
-kms = prompt("Enter the kms' number");
+kms = prompt("Enter the kilometers' number");
 // the user is asked his or her age
 age = prompt("Enter your age");
 // the ticket's base price is calculated
 price = kms * 0.21;
-// the discount is applyed, if possible
+// the discount is applied, if possible
 if (age < 18)
 {
   price *= 0.8;
@@ -21,5 +21,9 @@ else if (age > 65)
 {
   price *= 0.6;
 }
-// the price is rounded at two decimals and printed with the currency symbol
-document.writeln("The ticket's price is " + (Math.round(price * 100) / 100) + "€");
+// the price is rounded at two decimals and the currency symbol is added
+ price = (Math.round(price * 100) / 100) + "€";
+
+// the appropriate strings are inserted in the document
+document.getElementsByClassName("dear")[0].textContent = "Dear customer, your ticket's price is:";
+document.getElementsByClassName("price")[0].textContent = price;
