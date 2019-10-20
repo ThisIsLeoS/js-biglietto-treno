41467ff2aa39f5ba75ccd253b051d9d190ebd8e3
leos <leonardo.siri.email@gmail.com>
2019-10-20 12:08:35 +0200

    refactor code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* Calcolo prezzo biglietto del treno
 DESCRIZIONE:
-Il programma dovrà chiedere all’utente il numero di chilometri e l’età e dovrà calcolare il prezzo
-totale del viaggio. Il prezzo del biglietto è definito in base ai km (0.21 €cent al km) ma c’è uno
+Il programma dovrà chiedere all’utente il numero di chilometri e l’età e dovrà calcolare il prezzo 
+totale del viaggio. Il prezzo del biglietto è definito in base ai km (0.21 €cent al km) ma c’è uno 
 sconto del 20% per i minorenni e del 40% per gli over 65 */
 
 var kms, age, price, str;
@@ -15,11 +15,11 @@ price = kms * 0.21;
 // the discount is applyed, if possible
 if (age < 18)
 {
-  price = price / 100 * 80;
+  price *= 0.8;
 }
 else if (age > 65)
 {
-  price = price / 100 * 60;
+  price *= 0.6;
 }
 // the price is rounded at two decimals and printed with the currency symbol
 document.writeln("The ticket's price is " + (Math.round(price * 100) / 100) + "€");
